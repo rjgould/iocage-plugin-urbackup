5f2e0abc7ee64943d16892b45108dcd3393d664e
Tostis <tostis@email.com.invalid>
2023-03-25 20:11:16 +0100

    urbackup compiled from sources due to missing cryptopp library

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Iocage [UrBackup](https://www.urbackup.org/) plugin for TrueNas.
 
-Plugin built using the following instructions
-https://www.urbackup.org/freenasserverinstall.html
+Plugin built using the [official instructions](https://www.urbackup.org/server_source_install.html) forcing embedded crypto library.
 
 Map the folder /mnt/backups/urbackup, browse to http://jail-ip:55414 and configure an admin user.
--- post_install.sh
+++ post_install.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
+wget https://hndl.urbackup.org/Server/2.5.30/urbackup-server-2.5.30.tar.gz
+tar xf urbackup-server-2.5.30.tar.gz
+cd urbackup-server-2.5.30
+./configure --enable-embedded-cryptopp --enable-embedded-zstd
+make
+make install
+mkdir -p /usr/local/etc/rc.d
+mkdir -p /usr/local/etc/urbackup
+chmod u+x /usr/local/etc/rc.d/urbackup_server
 mkdir -p /mnt/backups/urbackup && chown urbackup:urbackup /mnt/backups/urbackup
 sysrc "urbackup_server_enable=YES"
 service urbackup_server start
